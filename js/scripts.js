7605be6d6a946e7c724effd69077776d854830e6
darrion gering and jaime takikawa <darrion@>
2019-03-28 14:13:23 -0700

    Add if else if and styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
-  
+  var height = parseInt(prompt("How tall are you in inches?"));
+  if(height <= 24) {
+    $(".rideDescription1").show();
+    $("#twister").addClass("green");
+    $("#rollerCoaster").addClass("red");
+    $("#logRide").addClass("red");
+    $("#teaCup").addClass("red");
+  } else if (height <= 48) {
+    $(".rideDescription1").show();
+    $(".rideDescription2").show();
+    $(".rideDescription3").show();
+    $("#twister").addClass("green");
+    $("#rollerCoaster").addClass("green");
+    $("#logRide").addClass("green");
+    $("#teaCup").addClass("green");
+  } else if (height <= 72) {
+    $(".rideDescription2").show();
+    $(".rideDescription3").show();
+    $(".rideDescription4").show();
+    $("#twister").addClass("red");
+    $("#rollerCoaster").addClass("green");
+    $("#logRide").addClass("green");
+    $("#teaCup").addClass("green");
+  } else {
+    alert("Invalid height! Please provide a number between 0 and 72");
+  }
+
 });
